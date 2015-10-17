8653982813c865ab735b01477f48116700d377c7
Zora <MrZoraman@Gmail.com>
2015-10-17 07:56:10 -0600

    Added TaskRepeater with its unittests. Time to start testing!

--- src/main/java/com/lagopusempire/lagoscheduler/TaskRepeater.java
+++ src/main/java/com/lagopusempire/lagoscheduler/TaskRepeater.java
@@ -2,8 +2,56 @@ package com.lagopusempire.lagoscheduler;
 
 public class TaskRepeater
 {
+    private final int delay;
+    private final int interval;
+    private final int repeats;
+    
+    private int tick = 0;
+    private int timesRun = 0;
+    private boolean willRunAgain = true;
+    
+    private boolean shouldRun = false;
+    
+    public TaskRepeater(int delay, int interval, int repeats)
+    {
+        this.delay = delay;
+        this.interval = interval;
+        this.repeats = repeats;
+    }
+    
+    public void cycle()
+    {
+        if(tick < delay)
+        {
+            shouldRun = false;
+            return;
+        }
+        
+        tick++;
+        
+        if(repeats > 0 && repeats > timesRun)
+        {
+            shouldRun = false;
+            willRunAgain = false;
+        }
+        else if(tick % interval == 0)
+        {
+            shouldRun = true;
+            timesRun++;
+        }
+        else
+        {
+            shouldRun = false;
+        }
+    }
+    
     public boolean shouldRun()
     {
-        return true;
+        return shouldRun;
+    }
+    
+    public boolean willRunAgain()
+    {
+        return willRunAgain;
     }
 }
---
+++ src/test/java/com/lagopusempire/lagoscheduler/TaskRepeaterTester.java
@@ -0,0 +1,149 @@
+package com.lagopusempire.lagoscheduler;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TaskRepeaterTester
+{
+    @Test
+    public void testRunOnce()
+    {
+        TaskRepeater repeater = new TaskRepeater(0, 0, 0);
+        repeater.cycle();
+        assertEquals(true, repeater.shouldRun());
+        repeater.cycle();
+        assertEquals(false, repeater.shouldRun());
+        assertEquals(false, repeater.willRunAgain());
+    }
+    
+    @Test
+    public void testDelay()
+    {
+        TaskRepeater repeater = new TaskRepeater(10, 0, 0);
+        for(int ii = 0; ii < 9; ii++)
+        {
+            repeater.cycle();
+        }
+        assertEquals(false, repeater.shouldRun());
+        repeater.cycle();
+        assertEquals(true, repeater.shouldRun());
+        repeater.cycle();
+        assertEquals(false, repeater.shouldRun());
+        assertEquals(false, repeater.willRunAgain());
+    }
+    
+    @Test
+    public void testInterval()
+    {
+        TaskRepeater repeater = new TaskRepeater(0, 10, -1);
+        for(int runs = 0; runs < 5; runs++)
+        {
+            for(int ii = 0; ii < 9; ii++)
+            {
+                repeater.cycle();
+            }
+            assertEquals(false, repeater.shouldRun());
+            repeater.cycle();
+            assertEquals(true, repeater.shouldRun());
+            assertEquals(true, repeater.willRunAgain());
+        }
+    }
+    
+    @Test
+    public void testRepeats()
+    {
+        TaskRepeater repeater = new TaskRepeater(0, 5, 3);
+        for(int ii = 0; ii < 4; ii++)
+        {
+            repeater.cycle();
+        }
+        assertEquals(false, repeater.shouldRun());
+        repeater.cycle();
+        assertEquals(true, repeater.shouldRun());
+        assertEquals(true, repeater.willRunAgain());
+        
+        for(int ii = 0; ii < 4; ii++)
+        {
+            repeater.cycle();
+        }
+        assertEquals(false, repeater.shouldRun());
+        repeater.cycle();
+        assertEquals(true, repeater.shouldRun());
+        assertEquals(true, repeater.willRunAgain());
+        
+        for(int ii = 0; ii < 4; ii++)
+        {
+            repeater.cycle();
+        }
+        assertEquals(false, repeater.shouldRun());
+        repeater.cycle();
+        assertEquals(true, repeater.shouldRun());
+        
+        assertEquals(false, repeater.willRunAgain());
+        
+        for(int ii = 0; ii < 4; ii++)
+        {
+            repeater.cycle();
+        }
+        assertEquals(false, repeater.shouldRun());
+        repeater.cycle();
+        assertEquals(false, repeater.shouldRun());
+        assertEquals(false, repeater.willRunAgain());
+    }
+    
+    @Test
+    public void testEverything()
+    {
+        TaskRepeater repeater = new TaskRepeater(5, 8, 3);
+        for(int ii = 0; ii < 5; ii++)
+        {
+            repeater.cycle();
+            assertEquals(false, repeater.shouldRun());
+            assertEquals(true, repeater.willRunAgain());
+        }
+        
+        for(int ii = 0; ii < 7; ii++)
+        {
+            repeater.cycle();
+            assertEquals(false, repeater.shouldRun());
+            assertEquals(true, repeater.willRunAgain());
+        }
+        
+        repeater.cycle();
+        assertEquals(true, repeater.shouldRun());
+        assertEquals(true, repeater.willRunAgain());
+        
+        for(int ii = 0; ii < 7; ii++)
+        {
+            repeater.cycle();
+            assertEquals(false, repeater.shouldRun());
+            assertEquals(true, repeater.willRunAgain());
+        }
+        
+        repeater.cycle();
+        assertEquals(true, repeater.shouldRun());
+        assertEquals(true, repeater.willRunAgain());
+        
+        for(int ii = 0; ii < 7; ii++)
+        {
+            repeater.cycle();
+            assertEquals(false, repeater.shouldRun());
+            assertEquals(true, repeater.willRunAgain());
+        }
+        
+        repeater.cycle();
+        assertEquals(true, repeater.shouldRun());
+        assertEquals(false, repeater.willRunAgain());
+        
+        for(int ii = 0; ii < 7; ii++)
+        {
+            repeater.cycle();
+            assertEquals(false, repeater.shouldRun());
+            assertEquals(false, repeater.willRunAgain());
+        }
+        
+        repeater.cycle();
+        assertEquals(false, repeater.shouldRun());
+        assertEquals(false, repeater.willRunAgain());
+    }
+}
