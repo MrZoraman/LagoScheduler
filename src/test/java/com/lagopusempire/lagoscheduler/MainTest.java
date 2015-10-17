b2beb7338741da819441160ea834732f3fb0d2db
Zora <MrZoraman@Gmail.com>
2015-10-17 12:57:43 -0600

    uh oh! those wrapper classes are causing shennanegans

--- src/test/java/com/lagopusempire/lagoscheduler/MainTest.java
+++ src/test/java/com/lagopusempire/lagoscheduler/MainTest.java
@@ -15,16 +15,27 @@ public class MainTest
     public static void main(String[] args)
     {
         printWithId("Hello from main thread!");
-        LagoScheduler scheduler = LagoScheduler.getInstance();
+        
+        TaskBehaviorHandler handler = new TaskBehaviorHandlerImpl("async");
+        
+        final LagoScheduler scheduler = LagoScheduler.getInstance();
         TaskRepeatInstructions repeatInstructions = new TaskRepeatInstructions(0, 20, -1);//once every second forever
-        scheduler.spawnAsyncTask(false, () -> {
+        final int asyncTid = scheduler.spawnAsyncTask(false, handler, () -> {
             printWithId("Hello from other thread!");
             return false;
         }, repeatInstructions);
+        
+        scheduler.spawnAsyncTask(false, () -> {
+            printWithId("sending 42 to other async thread!");
+            boolean received = scheduler.send(asyncTid, 42);
+            printWithId("Was my number received? " + received);
+            return false;
+        }, repeatInstructions.dupe());
+        
         scheduler.run();
     }
     
-    private class TaskBehaviorHandlerImpl
+    private static class TaskBehaviorHandlerImpl implements TaskBehaviorHandler
     {
         private final String name;
         
@@ -33,50 +44,28 @@ public class MainTest
             this.name = name;
         }
         
-        private <T> void notifyReceive(T[] stuff)
+        private void notifyReceive(Object[] stuff)
         {
             for(int ii = 0; ii < stuff.length; ii++)
             {
-                printWithId("recieved data! name=" + name + ", id=" + ii + ", data=" + stuff[ii].toString());
+                printWithId("recieved data! name=" + name + ", id=" + ii + ", data=" + stuff[ii]);
             }
         }
         
-        void onReceive(Integer[] i)
+        @Override
+        public void onReceive(Object[] stuff)
         {
-            notifyReceive(i);
-        }
-        
-        void onReceive(Double[] d)
-        {
-            notifyReceive(d);
-        }
-        
-        void onReceive(String[] s)
-        {
-            notifyReceive(s);
-        }
-        
-        void onReceive(Boolean[] b)
-        {
-            notifyReceive(b);
-        }
-        
-        void onReceive(Object[] o)
-        {
-            notifyReceive(o);
-        }
-        
-        void onReceive(int voids)
-        {
-            printWithId(voids + " voids received.");
+            notifyReceive(stuff);
         }
 
-        void onStart()
+        @Override
+        public void onStart()
         {
             printWithId("onStart");
         }
         
-        void onStop()
+        @Override
+        public void onStop()
         {
             printWithId("onStop");
         }
